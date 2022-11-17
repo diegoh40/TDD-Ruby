bd7fb3f93c45fc4f1f4d61f783e04f2335e32e94
mullenlucas <mullenlucas@gmail.com>
2022-11-17 17:51:10 -0300

    add reverser and fizzbuzz methods

--- factorial_functions.rb
+++ factorial_functions.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Solver class
 class Solver
   def factorial(num)
     if num.negative?
@@ -9,5 +12,20 @@ class Solver
       (1..num).reduce(1, :*)
     end
   end
+
+  def reverser(str)
+    str.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 15).zero?
+      'FizzBuzz'
+    elsif (num % 3).zero?
+      'Fizz'
+    elsif (num % 5).zero?
+      'Buzz'
+    else
+      num.to_s
     end
-  
+  end
+end
--- mock.rb
+++ mock.rb
@@ -1,25 +1,27 @@
+# frozen_string_literal: true
+
 require_relative './factorial_functions'
 
-describe Factorial do
-    it "finds the factorial of 5" do
-      calculator = Solver.new
-      expect(calculator.factorial(5)).to eq(120)
-    end
+describe Solver do
+  it 'finds the factorial of 5' do
+    calculator = Solver.new
+    expect(calculator.factorial(5)).to eq(120)
   end
+end
 
-  describe Reverser do
-    it "Check if the method reverse return a reverse string" do
-      reverser = Solver.new
-      expect(reverser.reverse('Hello')).to eq('olleH')
+describe Solver do
+  it 'Check if the method reverse return a reverse string' do
+    reverser = Solver.new
+    expect(reverser.reverser('Hello')).to eq('olleH')
   end
+end
 
-  describe FizzBuzz do
-    it "Check if the method reverse return a reverse string" do
-      fizzbuz = Solver.new
-      expect(fizzbuz.fizz(15)).to eq('FizzBuzz')
-      expect(fizzbuz.fizz(3)).to eq('Fizz')
-      expect(fizzbuz.fizz(5)).to eq('Buzz')
-      expect(fizzbuz.fizz(7)).to eq('7')
+describe Solver do
+  it 'Check if the method reverse return a reverse string' do
+    fizzbuz = Solver.new
+    expect(fizzbuz.fizzbuzz(15)).to eq('FizzBuzz')
+    expect(fizzbuz.fizzbuzz(3)).to eq('Fizz')
+    expect(fizzbuz.fizzbuzz(5)).to eq('Buzz')
+    expect(fizzbuz.fizzbuzz(7)).to eq('7')
   end
-
-  end
+end
