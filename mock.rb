cd1e24eb5215ddcd1ec9b9bea802557fac79a7f8
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-11-17 14:20:52 -0600

    Add mock reverse

--- mock.rb
+++ mock.rb
@@ -2,7 +2,16 @@ require_relative './factorial_functions'
 
 describe Factorial do
     it "finds the factorial of 5" do
-      calculator = Factorial.new
+      calculator = Solver.new
       expect(calculator.factorial(5)).to eq(120)
     end
+  end
+
+  describe Reverser do
+    it "Check if the method reverse return a reverse string" do
+      reverser = Solver.new
+      expect(reverser.reverse('Hello')).to eq('olleH')
+  end
+
+
   end
