6d941853dc91a1c173d0581180de822a2eab2685
Diego <godie8a@hotmail.com>
2022-11-17 15:17:10 -0500

    add factorial function

--- factorial_functions.rb
+++ factorial_functions.rb
@@ -1,2 +1,13 @@
+class Solver
+  def factorial(num)
+    if num.negative?
+      'Please use a positive number'
 
+    elsif [1, 0].include?(num)
+      1
+    else
+      (1..num).reduce(1, :*)
+    end
+  end
+    end
   
